e79d8a895e9d4ec649f163305d678c315606d36b
Matheusanjos0 <matheus.aguiar@sptech.school>
2026-03-05 16:39:00 -0300

    Codigo SQL com inserts e selects

--- pi trabalho.sql
+++ pi trabalho.sql
@@ -19,7 +19,7 @@ email Varchar(50) not null,
 cidade_e_estado varchar (70) not null
 );
 
-create table informcao_da_plantação (
+create table informacao_da_plantacao (
 id int primary key auto_increment,
 nome_da_empresa varchar(50) not null,
 area_de_plantacao_hectares int not null,
@@ -30,13 +30,38 @@ gasto_mensal_agua int not null
 create table dados_do_sensor (
 id int primary key auto_increment,
 nome_da_empresa Varchar (50) not null,
-data_horario_medicao datetime not null,
-porcentagem_umidade float (5,2) not null
+data_horario_medicao datetime default current_timestamp(),
+porcentagem_umidade decimal (5,2) not null
 );
 
--- Script de inserção de dados
+-- inserindo dados
+insert into cadastro values
+(default, 'JBS','2026-01-02','Ativo',1),
+(default, 'Raízen','2025-06-14','Ativo',1),
+(default, 'Agromania','2025-03-30','Cancelado',0),
+(default, 'Sítio Oliveira','2026-02-08','Teste',1),
+(default, 'Ferreira´s Agro','2026-01-11','Ativo',0);
 
-insert into cadastro(nome_da_empresa, data_cadastro,sts_plano, Aceite_dos_termos) values ('AgroSense', '2026-03-03', 'Ativo', 1);
+insert into informcao_do_cliente values
+(default,'JBS','1140028922','jbs@gmail.com','Guarulhos,SP'),
+(default,'Raízen','1198987676','raizen@gmail.com','Parati,RJ'),
+(default,'Agromania','1192987059','agromania@gmail.com','São Bernardo,SP'),
+(default,'Sítio Oliveira','1198388831','st.oliveira@gmail.com','Araraquara,SP'),
+(default,'Ferreira´s Agro','1198886611','ferreira.agro@gmail.com','São Carlos,RJ');
+
+insert into informcao_da_plantação values
+(default,'JBS',5,7500,5000),
+(default,'Raízen',10,15000,10000),
+(default,'Agromania',4,6800,4000),
+(default,'Sítio Oliveira',3,5000,3000),
+(default,'Ferreira´s Agro',8,10000,8000);
+
+insert into dados_do_sensor values
+(default,'JBS',default,54.01),
+(default,'Raízen',default,65.00),
+(default,'Agromania',default,77.00),
+(default,'Sítio Oliveira',default,68.50),
+(default,'Ferreira´s Agro',default,63.00);
 
 
 -- ----- Script de Query ------
@@ -46,8 +71,18 @@ else 'Não aceitou'
 end as termos_de_uso
 from cadastro;
 
-Select porcentagem_umidade, case
+Select *, case
 when porcentagem_umidade <65 THEN 'ALERTA DE SOLO SECO'
 When porcentagem_umidade >75 THEN 'ALERTA DE EXCESSO DE ÁGUA'
+else 'UMIDADE ESTÁ NO NÍVEL CORRETO'
 end as status_umidade
 FROM dados_do_sensor;
+
+
+select * from informcao_do_cliente;
+
+select * from infromcao_da_plantacao;
+
+
+
+
